7a1a7eb5fb3962b5d09b1e7e888c62f79d0d6c49
Jason Asariah <jasonsean.asariah@calbaptist.edu>
2026-09-19 15:52:38 -0700

    added stuffs

--- projects.js
+++ projects.js
@@ -1,16 +1,16 @@
 const projects = [
     {
-    title: "SpaceX - Starshield EE Team Projects",
+    title: "Starshield EE Team Projects",
     tags: ["SpaceX"],
     image: "Projects/Starshield.webp",
-    description: "Contributed to electrical engineering work supporting the Starshield program.",
+    description: "Designed prototype flight PCBs for the Starshield program.",
     link: "experience/spacex-starshield.html#starshield-projects"
   },
     {
-    title: "SpaceX - Dragon EE Team Projects",
+    title: "Dragon EE Team Projects",
     tags: ["SpaceX"],
     image: "Projects/dragon.jpg",
-    description: "Worked on avionics systems for spacecraft.",
+    description: "Designed harnesses and validation schemtics for Dragon program.",
     link: "experience/spacex-dragon.html#dragon-projects"
   },
   {
@@ -19,6 +19,13 @@ const projects = [
     image: "Projects/CubeSat.png",
     description: "Designing Attitude Determination Card and Electrical Power System PCBs for a 1U CubeSat.",
     link: "projects/cubesat.html"
+  },
+  {
+    title: "The Arsenal",
+    tags: ["CBU Robotics"],
+    image: "Projects/arsenal.png",
+    description: "Custom electronics system for sensor data acquisition for robot localization.",
+    link: "projects/arsenal-v1.html"
   },
   {
     title: "Quadruped Robot",
@@ -49,25 +56,11 @@ const projects = [
     link: "projects/emg-car.html"
   },
   {
-    title: "Thermal Protection Component Application Design",
-    tags: ["Bourns"],
-    image: "Projects/tco.png",
-    description: "Designed applications using thermal protection components.",
-    link: "experience/bourns.html#thermal-protection"
-  },
-  {
-    title: "High voltage Component Packaging",
+    title: "Engineering Team Projects",
     tags: ["Bourns"],
     image: "Projects/gdt.png",
     description: "Worked on high voltage protection component packaging.",
     link: "experience/bourns.html#high-voltage-packaging"
-  },
-  {
-    title: "CNN For Element Detection",
-    tags: ["CBU Robotics"],
-    image: "Projects/CNN.png",
-    description: "Developed CNN model to detect game elements using YOLO.",
-    link: "projects/cnn-element-detection.html"
   },
   {
     title: "NLP and DNNs on Tweet Datasets",
@@ -75,33 +68,5 @@ const projects = [
     image: "Projects/gis.png",
     description: "Developed ML models for geospatial tweet data analysis.",
     link: "projects/nlp-tweet-research.html"
-  },
-  {
-    title: "Passive Exoskeleton",
-    tags: ["CBU"],
-    image: "Projects/exo.png",
-    description: "Designed passive exoskeleton system.",
-    link: "projects/passive-exoskeleton.html"
-  },
-  {
-    title: "Game in C++",
-    tags: ["Personal"],
-    image: "Projects/astro.png",
-    description: "Developed a game using C++.",
-    link: "projects/cpp-game.html"
-  },
-  {
-    title: "Arsenal PCB",
-    tags: ["CBU Robotics"],
-    image: "Projects/noimg.png",
-    description: "Custom electronics system for robotics platform.",
-    link: "projects/arsenal-v1.html"
-  },
-  {
-    title: "MNIST Demo",
-    tags: ["Personal"],
-    image: "Projects/mnist.png",
-    description: "Neural network trained on MNIST dataset.",
-    link: "projects/mnist-demo.html"
   }
 ];
